e65977032f5e0a045c35fe1a8a11cedd4a95dd03
Unknown <oliveira.ambrozio@gmail.com>
2018-02-10 12:57:01 -0200

    port

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,4 @@
-const port = 80
+const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
@@ -11,7 +11,7 @@ server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryPaser())
 
-server.listen(port, function() {
+server.listen(process.env.PORT || port, function() {
     console.log(`backEnd is running is ${port}`)
 })
 
